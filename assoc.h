473d9258c6dad1127385710d2e5c8346e88733b9
leJTA <toulepiarmel@gmail.com>
2025-09-02 13:45:42 +0200

    Removed function 'update_item_in_hashtable' as it is no longer used

--- assoc.h
+++ assoc.h
@@ -14,8 +14,4 @@ void *assoc_get_iterator(void);
 bool assoc_iterate(void *iterp, item **it);
 void assoc_iterate_final(void *iterp);
 
-// BEGIN CODE (3Q)
-void update_item_in_hashtable(item* it);
-// END CODE (3Q)
-
 extern unsigned int hashpower;
